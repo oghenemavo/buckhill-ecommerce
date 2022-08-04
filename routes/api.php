cb5bb037082f13505bb4d421a15fae1e0a71393d
Mavi Onogomuho <mavi.onogomuho@titantrustbank.com>
2022-08-04 14:10:36 +0100

    setup larastan

--- routes/api.php
+++ routes/api.php
@@ -19,9 +19,12 @@ use App\Http\Controllers\AuthController;
 //     return $request->user();
 // });
 
-Route::controller(AuthController::class)->group(function () {
-    Route::post('login', 'login');
-    Route::post('register', 'register');
-    Route::post('logout', 'logout');
-    Route::post('refresh', 'refresh');
+Route::prefix('v1')->group(function() {
+    Route::controller(AuthController::class)->group(function () {
+        Route::post('login', 'login');
+        Route::post('register', 'register');
+        Route::post('logout', 'logout');
+        Route::post('refresh', 'refresh');
+    });
 });
+
